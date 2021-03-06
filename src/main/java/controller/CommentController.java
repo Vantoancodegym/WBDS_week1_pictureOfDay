18231c0f4eaca22e0420833a83de3fbc6e9c98e0
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-03-06 12:37:00 +0700

    done

--- src/main/java/controller/CommentController.java
+++ src/main/java/controller/CommentController.java
@@ -3,8 +3,7 @@ package controller;
 import model.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import service.ICommentService;
 
@@ -13,11 +12,34 @@ import java.util.List;
 @Controller
 @RequestMapping("pictures")
 public class CommentController {
+    @ModelAttribute("evaluates")
+    public String[] getEvaluateLít(){
+        String[] list = {"1","2","3","4","5"};
+        return list;
+    }
+
     @Autowired
     private ICommentService iCommentService;
     @GetMapping("")
     public ModelAndView showAll(){
         List<Comment> list= iCommentService.findAll();
-        return new ModelAndView("pictures");
+        ModelAndView modelAndView=new  ModelAndView("pictures");
+        modelAndView.addObject("list",list);
+        modelAndView.addObject("newComment",new Comment());
+        return modelAndView;
+    }
+    @PostMapping("create")
+    public ModelAndView create(@ModelAttribute Comment comment){
+        comment.setLikesAmount(0);
+        iCommentService.save(comment);
+        return new ModelAndView("redirect:/pictures");
+    }
+    @GetMapping("like")
+    public ModelAndView like(@RequestParam int id){
+        Comment comment=iCommentService.findById(id);
+        int likesAmount=comment.getLikesAmount()+1;
+        comment.setLikesAmount(likesAmount);
+        iCommentService.update(comment);
+        return new ModelAndView("redirect:/pictures");
     }
 }
